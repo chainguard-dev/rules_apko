6fd47e161ecabb1c07e7e5bbe3863d4901238b67
thesayyn <thesayyn@gmail.com>
2023-09-28 17:13:29 -0700

    feat: use apko resolve to generate locks

---
+++ scripts/update_locks.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -o errexit -o nounset -o pipefail
+
+external_apko=$(bazel cquery @rules_apko//apko:resolved_toolchain --output=files)
+output_base=$(bazel info output_base)
+apko="$output_base/$external_apko"
+
+
+for yaml in "./examples/"*"/apko.yaml" "./e2e/"*"/apko.yaml"; do
+  $apko resolve $yaml
+done
+
