092cf1f41c1e2e08eb3295468edefb17ca4ddf3b
Piotr (ptab) Tabor <132376425+sfc-gh-ptabor@users.noreply.github.com>
2023-11-29 15:16:41 +0100

    Enable: bazel run @rules_apko//apko resolve ./apko.yaml  (#36)
    Adds @rules_apko//apko(:apko) target that is executable and runs apko
    (resolved by toolchain) in the working directory.
    
    This simplify processes as avoids needs to install (and keep in sync)
    separate instance of apko outside of bazel.
    
    Please see the `./examples/resolve.sh` file for the examplar usage.
    
    ---------
    
    Signed-off-by: Piotr Tabor <piotr.tabor@snowflake.com>
    Signed-off-by: Jason Hall <jason@chainguard.dev>
    Co-authored-by: Jason Hall <jason@chainguard.dev>

--- examples/resolve.sh
+++ examples/resolve.sh
@@ -2,9 +2,4 @@
 
 set -e -x
 
-for d in $(find . -name apko.yaml); do
-  (
-    cd $(dirname ${d})
-    docker run -v "$PWD":/work cgr.dev/chainguard/apko@sha256:d5e219c1ceb2e7d56a5933df54819467e5b3331098ea8bebc996fdd30f974f33 resolve /work/apko.yaml
-  )
-done
+find . -name apko.yaml | xargs bazel run @rules_apko//apko resolve
