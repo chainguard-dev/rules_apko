806c72a7ce3113d5e11108ef02f5a83fd9c4ff19
Alex Eagle <alex@aspect.dev>
2025-09-02 16:05:40 -0700

    chore: code review comments

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -28,30 +28,3 @@ rules_apko requires a one-time setup to configure bazel to be able to make parti
 Follow https://github.com/chainguard-dev/rules_apko/blob/main/docs/initial-setup.md for the setup.
 
 EOF
-
-cat << EOF
-## Using Bzlmod with Bazel 6 or above
-
-1. (Bazel 6 only) Enable with \`common --enable_bzlmod\` in \`.bazelrc\`.
-2. Add to your \`MODULE.bazel\` file:
-
-\`\`\`starlark
-bazel_dep(name = "rules_apko", version = "${TAG:1}")
-\`\`\`
-
-## Using WORKSPACE
-
-Paste this snippet into your `WORKSPACE.bazel` file:
-
-\`\`\`starlark
-load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
-http_archive(
-    name = "rules_apko",
-    sha256 = "${SHA}",
-    strip_prefix = "${PREFIX}",
-    url = "https://github.com/chainguard-dev/rules_apko/releases/download/${TAG}/${ARCHIVE}",
-)
-EOF
-
-awk 'f;/--SNIP--/{f=1}' e2e/smoke/WORKSPACE.bazel
-echo "\`\`\`" 
