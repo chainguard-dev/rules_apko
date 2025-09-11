6b06b60ac7c3199ee3911b7d1367cac081174cf3
Dimitri John Ledkov <dimitri.ledkov@chainguard.dev>
2025-09-11 20:40:01 +0100

    bcr: add docs
    - Drop no-op bcr/config.yml.
    - Add docs to the template.json
    - Drop obsolete / out-of-date release boilerplate
    - Add docs in release_prep.sh
    
    Based on recommendations from:
    - https://github.com/chainguard-dev/rules_apko/pull/176

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -11,39 +11,10 @@ ARCHIVE="rules_apko-$TAG.tar.gz"
 git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip > $ARCHIVE
 SHA=$(shasum -a 256 $ARCHIVE | awk '{print $1}')
 
-cat << EOF
-
-## Initial setup (when using with Bazel < 7.1)
-
-rules_apko requires a one-time setup to configure bazel to be able to make partial fetches.
-
-Follow https://github.com/chainguard-dev/rules_apko/blob/main/docs/initial-setup.md for the setup.
-
-EOF
-
-cat << EOF
-## Using Bzlmod
-
-1. Enable with \`common --enable_bzlmod\` in \`.bazelrc\`.
-2. Add to your \`MODULE.bazel\` file:
-
-\`\`\`starlark
-bazel_dep(name = "rules_apko", version = "${TAG:1}")
-\`\`\`
-
-## Using WORKSPACE
-
-Paste this snippet into your `WORKSPACE.bazel` file:
-
-\`\`\`starlark
-load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
-http_archive(
-    name = "rules_apko",
-    sha256 = "${SHA}",
-    strip_prefix = "${PREFIX}",
-    url = "https://github.com/chainguard-dev/rules_apko/releases/download/${TAG}/${ARCHIVE}",
-)
-EOF
-
-awk 'f;/--SNIP--/{f=1}' e2e/smoke/WORKSPACE.bazel
-echo "\`\`\`" 
+# Add generated API docs to the release, see https://github.com/bazelbuild/bazel-central-registry/issues/5593
+docs="$(mktemp -d)"; targets="$(mktemp)"
+bazel --output_base="$docs" query --output=label --output_file="$targets" 'kind("starlark_doc_extract rule", //...)'
+bazel --output_base="$docs" build --target_pattern_file="$targets" --remote_download_regex='.*doc_extract\.binaryproto'
+tar --create --auto-compress \
+    --directory "$(bazel --output_base="$docs" info bazel-bin)" \
+    --file "$GITHUB_WORKSPACE/${ARCHIVE%.tar.gz}.docs.tar.gz" .
